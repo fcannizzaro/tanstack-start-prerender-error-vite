10771542ef51851fbcfaf50e2504c2d8f7a76907
Francesco Saverio Cannizzaro <francescosaverio.cannizzaro@gmail.com>
2025-11-23 16:50:09 +0100

    init

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
+
+const config = defineConfig({
+  plugins: [
+    devtools(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tailwindcss(),
+    tanstackStart({
+      spa: {
+        enabled: true,
+      },
+    }),
+    react(),
+    nitroV2Plugin({
+      preset: "node-server",
+      compatibilityDate: "latest",
+    }),
+  ],
+});
+
+export default config;
